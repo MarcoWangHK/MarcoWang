0ce50bc6ead3fd7e1d83e6e3b8a85c7843e32e95
Marco Wang <marcowang@seekasia.com>
2016-03-03 09:48:34 +0800

    no message

---
+++ Marco-TestLayer/metadata.rb
@@ -0,0 +1,6 @@
+name 'MarcoTestLayer'
+maintainer 'Marco Wang'
+maintainer_email 'marcowang@seekasia.com'
+source_url 'https://github.com/MarcoWangHK/MarcoWang' if respond_to?(:source_url)
+system 'ubuntu'
+version '14.04'
