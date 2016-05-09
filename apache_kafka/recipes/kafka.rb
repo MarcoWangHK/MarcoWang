a1f1f39ff2f7a9121aeb2581a47c3cdf9efb2a82
Marco Wang <marcowang@seekasia.com>
2016-05-09 16:03:39 +0800

    no message

---
+++ apache_kafka/recipes/kafka.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: apache kafka
+# Recipe:: kafka
+#
+#
+
+directory "/home/kafka/Downloads" do
+	owner 'root'
+	group 'root'
+	mode 0755
+    action :create
+end
+
+
+execute "download-kafka" do
+  command "wget http://ftp.cuhk.edu.hk/pub/packages/apache.org/kafka/0.8.2.2/kafka_2.10-0.8.2.2.tgz -O /home/kafka/Downloads/kafka.tgz"
+  action :run
+end
+
+directory "/opt/kafka" do
+	owner 'root'
+	group 'root'
+	mode 0755
+    action :create
+end
+
+execute "cd" do
+  command "cd /opt/kafka"
+  command "sudo tar -xvzf /home/kafka/Downloads/kafka.tgz --strip 1"
+  action :run
+end
