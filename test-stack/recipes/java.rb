56e2824c4b4b3be9d6c1712b4f6ecc90ec0bfa2e
Marco Wang <marcowang@seekasia.com>
2016-05-09 14:18:23 +0800

    no message

--- test-stack/recipes/java.rb
+++ test-stack/recipes/java.rb
@@ -9,17 +9,9 @@ execute "update-upgrade" do
   action :run
 end
 
-execute "java1" do
+execute "java-install" do
   command "sudo add-apt-repository -y ppa:webupd8team/java"
-  action :run
-end
-
-execute "java2" do
   command "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections"
-  action :run
-end  
-
-execute "java3" do
   command "sudo apt-get -y install oracle-java8-installer"
   action :run
-end  
+end
