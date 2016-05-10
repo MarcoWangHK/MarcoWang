aa04936f80485f4712de0d21174b724757d41fbc
Marco Wang <marcowang@seekasia.com>
2016-05-10 11:35:43 +0800

    no message

--- test-stack/recipes/install.rb
+++ test-stack/recipes/install.rb
@@ -7,4 +7,4 @@
 #include_recipe 'test-stack::java'
 include_recipe 'test-stack::zookeeper'
 include_recipe 'test-stack::kafka'
-include_recipe 'kafka_config.rb'
+include_recipe 'test-stack::kafka_config'
