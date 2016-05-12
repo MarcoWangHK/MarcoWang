8e70669ea2c5af0743248c23dc0ddb856bacf71b
Marco Wang <marcowang@seekasia.com>
2016-05-12 10:40:48 +0800

    no message

--- kafkatest/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: kafka
-# Recipe:: default
-#
-
-include_recipe 'kafkatest::_defaults'
-include_recipe 'kafkatest::_setup'
-include_recipe 'kafkatest::_install'
-include_recipe 'kafkatest::_configure'
