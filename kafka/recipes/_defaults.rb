67c6074dd212310d6ac0bbd3b29d477bcb34c122
Marco Wang <marcowang@seekasia.com>
2016-05-20 15:03:05 +0800

    no message

--- kafka/recipes/_defaults.rb
+++ kafka/recipes/_defaults.rb
@@ -19,8 +19,8 @@ unless broker_attribute?(:iothreads)
   node.default.kafka.broker.num.io.threads = 8
 end
 
-unless broker_attribute?(:deletetopicenable)
-  node.default.kafka.broker.delete.topic.enable = node.kafka.version
+unless broker_attribute
+  node.default.kafka.broker.delete.topic.enable = true
 end
 
 unless node.kafka.gc_log_opts
