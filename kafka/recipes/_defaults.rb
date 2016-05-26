7adc1804079073260d5fb4d6bf410470da7e7496
Marco Wang <marcowang@seekasia.com>
2016-05-26 11:26:55 +0800

    no message

--- kafka/recipes/_defaults.rb
+++ kafka/recipes/_defaults.rb
@@ -35,6 +35,10 @@ unless broker_attribute?(:deletetopicenable)
   node.default.kafka.broker.delete.topic.enable = 'true'
 end
 
+unless broker_attribute?(:deletetopicenable)
+  node.default.kafka.broker.delete.topic.enable = 1
+end
+
 unless node.kafka.gc_log_opts
   node.default.kafka.gc_log_opts = %W[
     -Xloggc:#{::File.join(node.kafka.log_dir, 'kafka-gc.log')}
